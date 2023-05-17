bd1524269cce2e02325791375adb6ec2f8442f4c
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-05-17 18:20:55 +0600

    create about section with image relative position

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         <li><Link to='/about'>About</Link></li>
     </>
     return (
-        <div className="navbar bg-base-100 h-28 mb-4">
+        <div className="navbar bg-base-100 h-28 mb-8">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
