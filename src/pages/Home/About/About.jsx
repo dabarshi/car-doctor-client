03d9d4cd4d37f46d7283644b739e5b7e0c397798
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-05-17 20:54:08 +0600

    load services fake data and display service card

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -12,8 +12,8 @@ const About = () => {
                 <div className='lg:w-1/2'>
                     <h3 className='text-xl text-orange-700 font-bold'>About Us</h3>
                     <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
-                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-                    <p className="py-6">The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+                    <p className="py-6">The majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</p>
                     <button className="btn btn-warning">Get More Info</button>
                 </div>
             </div>
